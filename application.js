7dc4b161679f392e9b7d87b1abe8c8c2b3a8de9b
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2014-12-05 15:39:56 -0800

    Updated Nav Bar highlight of current page to work when clicking from bottom to top. Changes color scheme

--- application.js
+++ application.js
@@ -18,65 +18,88 @@ function bindEvents() {
 
 function landingHover() {
   $('#landing-nav').css({'color': '#2f4f4f'})
+  // $('#landing-nav-li').css({'background-color': 'white'})
 };
 
 function landingHoverReverse() {
   $('#landing-nav').css({'color': 'white'})
+  // $('#landing-nav-li').css({'background-color': 'black'})
 };
 
 
 
 function profileHover() {
   $('#profile-nav').css({'color': '#2f4f4f'})
+  // $('#profile-nav-li').css({'background-color': 'white'})
 }
 
 function profileHoverReverse() {
   $('#profile-nav').css({'color': 'white'})
+  // $('#profile-nav-li').css({'background-color': 'black'})
 }
 
 
 
 function experienceHover() {
   $('#experience-nav').css({'color': '#2f4f4f'})
+  // $('#experience-nav-li').css({'background-color': 'white'})
 }
 
 function experienceHoverReverse() {
   $('#experience-nav').css({'color': 'white'})
+  // $('#experience-nav-li').css({'background-color': 'black'})
 }
 
 
 
 function projectsHover() {
   $('#projects-nav').css({'color': '#2f4f4f'})
+  // $('#projects-nav-li').css({'background-color': 'white'})
 }
 
 function projectsHoverReverse() {
   $('#projects-nav').css({'color': 'white'})
+  // $('#projects-nav-li').css({'background-color': 'black'})
 }
 
 
 
 function contactHover() {
   $('#contact-nav').css({'color': '#2f4f4f'})
+  // $('#contact-nav-li').css({'background-color': 'white'})
 }
 
 function contactHoverReverse() {
   $('#contact-nav').css({'color': 'white'})
+  // $('#contact-nav-li').css({'background-color': 'black'})
 }
 
 
 function createWaypoints() {
-  $('#landing').waypoint(highlight)
-  $('#profile').waypoint(highlight)
-  $('#experience').waypoint(highlight)
-  $('#projects').waypoint(highlight)
-  $('#contact').waypoint(highlight)
+  $('#landing').waypoint(highlightTop)
+  $('#landing-bot').waypoint(highlightBottom)
+  $('#profile').waypoint(highlightTop)
+  $('#profile-bot').waypoint(highlightBottom)
+  $('#experience').waypoint(highlightTop)
+  $('#experience-bot').waypoint(highlightBottom)
+  $('#projects').waypoint(highlightTop)
+  $('#projects-bot').waypoint(highlightBottom)
+  $('#contact').waypoint(highlightTop)
+  $('#contact-bot').waypoint(highlightBottom)
+
 }
 
-function highlight() {
-  $('#nav').children().css({'background-color': 'black'}, {offset:'50%'})
+function highlightTop() {
+  $('#nav').children().css({'background-color': 'black'})
   var a = $(this).attr('id')
-  $('#'+a+'-nav-li').css({'background-color': 'purple'}, {offset:'50%'})
+  $('#'+a+'-nav-li').css({'background-color': 'purple'})
+}
+
+function highlightBottom() {
+  $('#nav').children().css({'background-color': 'black'})
+  var a = $(this).attr('id')
+  a = a.substring(0, a.length - 4);
+  $('#'+a+'-nav-li').css({'background-color': 'purple'})
 }
 
 
